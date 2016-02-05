7e5a8f2a803d34fa0d7a8eab4195a0749eeaa273
Thomas Stringer <sqlsalt@outlook.com>
2016-02-05 10:42:00 -0800

    Added cli functionality

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 const logger = require('azure-logger');
+const program = require('commander');
 
+console.log(' *** azure logger ***');
+
+program
+  .version('1.0.0')
+  .option('-a, --account <accountName>', 'Azure storage account name')
+  .option('-k, --key <key>', 'Storage key')
+  .option('-t, --table <table>', 'Table name')
+  .parse(process.argv);
+
+const options = {
+  cred: {
+    accountName: program.account,
+    accountKey: program.key
+  }
+};
+
+if (program.table) {
+  options.table = program.table;
+}
+
+logger.get(options, function (err, entries) {
+  console.log(`entry count :: ${entries.length}`);
+});
