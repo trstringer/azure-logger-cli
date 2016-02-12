fa33bfa8efeb5ae216ed8d6861c899dc05186b3b
Thomas Stringer <tr.stringer@outlook.com>
2016-02-11 20:59:40 -0500

    Added support for env vars for azure storage account and key

--- app.js
+++ app.js
@@ -15,22 +15,29 @@ program
   .option('-s, --search <search>', 'Search string')
   .parse(process.argv);
 
+const account = program.account || process.env.AZURE_STORAGE_ACCOUNT;
+const key = program.key || process.env.AZURE_STORAGE_KEY;
 
-
-const options = {
-  cred: {
-    accountName: program.account,
-    accountKey: program.key
-  }
-};
-
-if (program.table) {
-  options.table = program.table;
+if (!account || !key) {
+  console.log('You must specify the --account [or AZURE_STORAGE_ACCOUNT env var] and --key [or AZURE_STORAGE_KEY env var]');
+  console.log('Exiting...');
 }
+else {
+  const options = {
+    cred: {
+      accountName: account,
+      accountKey: key
+    }
+  };
 
-logger.get(options, function (err, entries) {
-  var i;
-  for (i = 0; i < entries.length; i++) {
-    displayEntry(entries[i]);
+  if (program.table) {
+    options.table = program.table;
   }
-});
+
+  logger.get(options, function (err, entries) {
+    var i;
+    for (i = 0; i < entries.length; i++) {
+      displayEntry(entries[i]);
+    }
+  });
+}
